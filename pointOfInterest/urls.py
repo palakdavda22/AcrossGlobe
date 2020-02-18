97b022d04881f7f14e4d46a88b18132da68f9d94
palak davda <palakdavda2202@gmail.com>
2020-02-18 22:49:41 +0530

    route generation start

--- pointOfInterest/urls.py
+++ pointOfInterest/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
 	path('findPlaces/', views.findPlaces, name="findPlaces"),
 	path('places/', views.places, name="places"),
+	path('days/', views.DaysForRoute, name="places"),
+
 	# path('shopping/', views.shopping, name="shopping")
 ]
